6cc5cb8bdcc0a00d06243ae55e0ebd55bb3846ee
GitHub Action <action@github.com>
2025-08-25 00:21:22 +0000

    Initialize template repository proper
    Set package name to com.github.atmosphereclient.skylight
    Set project name to skylight
    Set modid to skylight

--- src/main/java/com/github/atmosphereclient/skylight/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/atmosphereclient/skylight/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.atmosphereclient.skylight.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
